811411f5d7d12d93aab557f321f3e17750008df0
Daniel Kessel <d.kessel@mailbox.org>
2018-07-28 20:39:47 +0200

    add gluon_build_all.sh and travis.yml customizations

---
+++ gluon_build_all.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+set -e
+BUILD_LOG=gluon-build.log
+BUILD_HOME=~/gluon-build
+GLUON_RELEASE=v2016.2.7
+SITE_RELEASE=2016
+MY_GLUON_SITE=ffhnx
+SITE_DIR=site-$MY_GLUON_SITE
+
+echo ok, go &> $BUILD_LOG
+
+GL_FOLDER=$BUILD_HOME/gluon
+SI_FOLDER=$BUILD_HOME/$SITE_DIR
+
+#altes zeug weg:
+rm -rf $BUILD_HOME &>> $BUILD_LOG
+
+mkdir -p $BUILD_HOME &>> $BUILD_LOG
+cd $BUILD_HOME &>> $BUILD_LOG
+# check out everything
+git clone https://github.com/freifunk-gluon/gluon.git gluon -b $GLUON_RELEASE &>> $BUILD_LOG
+git clone https://github.com/FreifunkHuenxe/site-ffhnx.git $SITE_DIR -b $SITE_RELEASE &>> $BUILD_LOG
+
+# copy some customizations:
+cp custom-build/mymodules $GL_FOLDER/modules &>> $BUILD_LOG
+cp custom-build/mysite.mk $SI_FOLDER/site.mk &>> $BUILD_LOG
+
+# build everything
+BRANCH=stable
+
+cd $GL_FOLDER #1>> $BUILD_LOG
+
+SITE="site-$(cat $SI_FOLDER/site.conf |grep site_code|awk {'print $3'}|sed "s/'//g"|sed "s/,//g")"
+make update GLUON_SITEDIR=$SI_FOLDER &>> $BUILD_LOG # Get other repositories used by Gluon
+
+#clear
+echo "== Building $SI_FOLDER" &>> $BUILD_LOG
+
+BINDIR=$(cat $SI_FOLDER/site.conf |grep site_code|awk {'print $3'}|sed "s/'//g"|sed "s/,//g")
+GLUON_IMAGEDIR=$BUILD_HOME/images_$GLUON_RELEASE/$BINDIR
+echo "== calling make for $SITE" &>> $BUILD_LOG
+make GLUON_BRANCH=$BRANCH GLUON_SITEDIR=$SI_FOLDER GLUON_TARGET=ar71xx-generic V=s &>> $BUILD_LOG
+## make GLUON_BRANCH=$BRANCH GLUON_SITEDIR=$SI_FOLDER GLUON_TARGET=ar71xx-nand
+## make GLUON_BRANCH=$BRANCH GLUON_SITEDIR=$SI_FOLDER GLUON_TARGET=mpc85xx-generic
+## make GLUON_BRANCH=$BRANCH GLUON_SITEDIR=$SI_FOLDER GLUON_TARGET=x86-generic
+## make GLUON_BRANCH=$BRANCH GLUON_SITEDIR=$SI_FOLDER GLUON_TARGET=x86-kvm_guest
+## make manifest GLUON_BRANCH=$BRANCH GLUON_SITEDIR=$SI_FOLDER
+echo "done"
